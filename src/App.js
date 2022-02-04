3b904b3f652b8310a4f72771bc8bb100d8ab01e0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-04 15:01:34 -0500

    Add components and boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Info from './components/Info';
+import About from './components/About';
+import Footer from './components/Footer';
+import Interests from './components/Interests';
 
 const App = () => {
     return (
         <div>
-            App
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
         </div>
     );
 }
 
 export default App;
+
+// components for
+// info (photo, name, buttons, etc.)
+// about
+// interests
+// footer
